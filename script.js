4b88d9705e5174e90c5387176af6cf7a57c6164c
poojad2101 <davepooja2101@gmail.com>
2022-05-26 02:04:13 +0530

    fixed the issue of rock going off the game screen

--- script.js
+++ script.js
@@ -2,14 +2,32 @@ const dino = document.getElementById("dino");
 const rock = document.getElementById("rock");
 const score = document.getElementById("score");
 
+
 function jump() {
   dino.classList.add("jump-animation");
   setTimeout(() =>
-    dino.classList.remove("jump-animation"), 500);
+    dino.classList.remove("jump-animation"), 450);
 }
 
-document.addEventListener('keypress', (event) => {
-  if (!dino.classList.contains('jump-animation')) {
+
+document.addEventListener("keypress", function (event) {
+  
     jump();
   }
-})
+)
+
+setInterval(() => {
+    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+console.log(dinoTop)
+
+const rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
+console.log(rockLeft)
+
+if(rockLeft < 0) {
+    rock.style.display = "none";
+} else {
+    rock.style.display = '';
+}
+}, 50);
+
+
