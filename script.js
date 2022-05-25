3a0ca34193effe4212d084632e52f501f6b442da
poojad2101 <davepooja2101@gmail.com>
2022-05-25 11:48:59 +0530

    created jump function

---
+++ script.js
@@ -0,0 +1,15 @@
+const dino = document.getElementById("dino");
+const rock = document.getElementById("rock");
+const score = document.getElementById("score");
+
+function jump() {
+  dino.classList.add("jump-animation");
+  setTimeout(() =>
+    dino.classList.remove("jump-animation"), 500);
+}
+
+document.addEventListener('keypress', (event) => {
+  if (!dino.classList.contains('jump-animation')) {
+    jump();
+  }
+})
